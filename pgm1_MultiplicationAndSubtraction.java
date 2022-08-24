edf9b7cd457f2ab23e831ce23243aef2283278bc
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-08-24 18:10:12 +0530

    Update pgm1_MultiplicationAndSubtraction.java

--- pgm1_MultiplicationAndSubtraction.java
+++ pgm1_MultiplicationAndSubtraction.java
@@ -1,19 +1,20 @@
 import java.util.*;
-	public class Main
+public class Main
+{
+	public static void main(String[] args) 
 	{
-		public static void main(String[] args) {
-	   	 Scanner sc=new Scanner(System.in);
-	    	int N=sc.nextInt();
-	    	System.out.println("Multiplication table");
-	    	for(int i=1;i<=20;i++)
-	    	{
-	        	System.out.println(N+" * "+i+"="+N*i);
-	   	 }
-	    
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.println("Multiplication table");
+		for(int i=1;i<=20;i++)
+		   {
+		     System.out.println(N+" * "+i+"="+N*i);
+		    }
+
 		System.out.println("Subtraction table");
 		for(int j=N;j<=20;j++)
 		{
-		    System.out.println(j+" - "+N+"="+(j-N));
+		   System.out.println(j+" - "+N+"="+(j-N));
 		}
 	}
-}
+      }
