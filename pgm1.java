375dc13ec3fe35083f73f7eae10ebd5c714c957d
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-08-24 19:11:11 +0530

    Add files via upload

---
+++ pgm1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+	public class Main
+	{
+		public static void main(String[] args) {
+	   	 Scanner sc=new Scanner(System.in);
+	    	int N=sc.nextInt();
+	    	System.out.println("Multiplication table");
+	    	for(int i=1;i<=20;i++)
+	    	{
+	        	System.out.println(N+" * "+i+"="+N*i);
+	   	 }
+	    
+		System.out.println("Subtraction table");
+		for(int j=N;j<=20;j++)
+		{
+		    System.out.println(j+" - "+N+"="+(j-N));
+		}
+	}
+}
