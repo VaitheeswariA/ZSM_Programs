6a02908f0feb8cc9fb07227b4eed7301f5bd393b
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-08-24 18:23:52 +0530

    Update pgm3.java

--- pgm3.java
+++ pgm3.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
+	 /*Difference between the given number and its reverse*/
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
 	    int rem=0,num=0,n1=n;
