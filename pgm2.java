e2c9ca16cd4f20c7bd6a39f1d45bbb6d8459eb4d
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-08-24 18:18:20 +0530

    Add files via upload

---
+++ pgm2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    char c=sc.next().charAt(0);
+	    if(c>=65 && c<=90)
+	    {
+    	    for(int i=c;i<=90;i++)
+    	    {
+    	        System.out.print((char)i);
+    	    }
+	    }
+	    else
+	    {
+	        for(int i=c;i<=122;i++)
+	        {
+	            System.out.print((char)i);
+	        }
+	    
+	    }
+	    
+	    
+	}
+}
